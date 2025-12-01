fc0ab9390bb3474daabb834d08c5982ef34b63c7
Blake Knight <oss.ideas@gmail.com>
2025-12-01 15:12:13 -0600

    🔀 update type; AoC only has 12 days this year

--- utils/typescript/aoc.ts
+++ utils/typescript/aoc.ts
@@ -2,32 +2,7 @@ import { unindent } from "@blakek/scratchpad";
 import * as Bun from "bun";
 import * as path from "path";
 
-type PuzzleDay =
-  | 1
-  | 2
-  | 3
-  | 4
-  | 5
-  | 6
-  | 7
-  | 8
-  | 9
-  | 10
-  | 11
-  | 12
-  | 13
-  | 14
-  | 15
-  | 16
-  | 17
-  | 18
-  | 19
-  | 20
-  | 21
-  | 22
-  | 23
-  | 24
-  | 25;
+type PuzzleDay = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 const mutableState = {
   exampleInput: "",
